dfd1b80b818a0335fbc93abc37ca8152375532b5
Neralem <49270443+Neralem@users.noreply.github.com>
2022-09-03 12:42:20 +0200

    Projektdateien hinzufügen.

---
+++ CustomSplashBugTest/Platforms/Tizen/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace CustomSplashBugTest;
+
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
+}
